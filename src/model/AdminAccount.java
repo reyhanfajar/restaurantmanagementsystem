c6401be3c1b7e0850fc20e9033467b5ac2089db0
imamazka <70789745+imamazka@users.noreply.github.com>
2021-12-26 15:19:01 +0700

    Add files via upload
    Login Scene

---
+++ src/model/AdminAccount.java
@@ -0,0 +1,20 @@
+package com.example.restaurantmanagementsystem.Model;
+
+public class AdminAccount {
+
+    private String userName;
+    private String password;
+
+    public AdminAccount (String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
