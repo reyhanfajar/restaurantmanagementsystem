0e39fa00702dfca1eb238085542df67c854a13c8
Alvitoikrm <85780874+Alvitoikrm@users.noreply.github.com>
2021-12-27 00:03:01 +0700

    make scene changes
    not fix yet

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,13 +1,23 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.AdminAccount;
 
 public class LoginController {
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
     @FXML
     private TextField usernameText;
     
@@ -25,7 +35,7 @@ public class LoginController {
     String password;
 
     @FXML
-    protected void onLoginButtonClick() {
+    protected void onLoginButtonClick() throws IOException {
         // Fake account
         AdminAccount adminAccount = new AdminAccount("admin", "admin");
 
@@ -36,9 +46,10 @@ public class LoginController {
         if (username.equals(adminAccount.getUserName()) && password.equals(adminAccount.getPassword())) {
             labelText.setVisible(false);
             // Change scene
-            //
-            //
-            
+            root = FXMLLoader.load(getClass().getResource("Feature.fxml"));
+    		scene = new Scene(root);
+    		stage.setScene(scene);
+    		stage.show();
             
         }
         // Jika input kosong
@@ -51,5 +62,10 @@ public class LoginController {
             labelText.setVisible(true);
             labelText.setText("Entered Information is Wrong, Try Again!");
         }
+        
+        
+        
+        
+        
     }
 }
